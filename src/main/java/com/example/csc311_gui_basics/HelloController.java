ed0b33db3ef63aec8e12d78b280abf2aad0b7160
Jaskaran Bhogal <bhogalj03@gmail.com>
2026-09-18 22:05:14 -0400

    Style About Me page with dark theme and portfolio buttons

--- src/main/java/com/example/csc311_gui_basics/HelloController.java
+++ src/main/java/com/example/csc311_gui_basics/HelloController.java
@@ -3,12 +3,19 @@ package com.example.csc311_gui_basics;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Thanks for visiting my About Me page!");
+    protected void onViewWorkButtonClick() {
+        try {
+            Desktop.getDesktop().browse(new URI("https://github.com/jaskaranbhogal"));
+        } catch (Exception e) {
+            welcomeText.setText("GitHub: github.com/jaskaranbhogal");
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.csc311_gui_basics {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
 
     opens com.example.csc311_gui_basics to javafx.fxml;
     exports com.example.csc311_gui_basics;

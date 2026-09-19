1ff3fdb568f4b90ce0f67d00c690463404e360f6
Jaskaran Bhogal <bhogalj03@gmail.com>
2026-09-18 21:42:46 -0400

    Add centered About Me details, degree progress bar, and button

--- src/main/java/com/example/csc311_gui_basics/HelloController.java
+++ src/main/java/com/example/csc311_gui_basics/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Thanks for visiting my About Me page!");
     }
 }
